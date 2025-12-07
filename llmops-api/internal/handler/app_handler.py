cfee662f9713ed88e640b0eab1e93f8fefeb89f9
root <yin_991@163.com>
2025-12-07 20:23:42 +0800

    feat 为LLMOps添加记忆功能

--- llmops-api/internal/handler/app_handler.py
+++ llmops-api/internal/handler/app_handler.py
@@ -9,11 +9,15 @@
 import os
 import uuid
 from dataclasses import dataclass
+from operator import itemgetter
 from uuid import UUID
 
 from injector import inject
+from langchain.memory import ConversationBufferWindowMemory
+from langchain_community.chat_message_histories import FileChatMessageHistory
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 from langchain_openai import ChatOpenAI
 
 from internal.exception import FailException
@@ -52,20 +56,37 @@ class AppHandler:
         if not req.validate():
             return validate_error_json(req.errors)
 
-        # 2. 构建组件
-        prompt = ChatPromptTemplate.from_template("{query}")
+        # 2. 构建prompt和记忆
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", "你是一个强大的聊天机器人，能根据用户的提问回复对应的问题"),
+            MessagesPlaceholder("history"),
+            ("human", "{query}")
+        ])
+        memory = ConversationBufferWindowMemory(
+            k=3,
+            input_key="query",
+            return_messages=True,
+            output_key="output",
+            chat_memory=FileChatMessageHistory("./storage/memory/chat_history.txt")
+        )
+
+        # 3. 创建llm
         llm = ChatOpenAI(
             model="gpt-4",
             api_key=os.getenv("GPTSAPI_API_KEY"),
             base_url=os.getenv("OPENAI_API_BASE"),
         )
-        parser = StrOutputParser()
 
-        # 3. 构建链
-        chain = prompt | llm | parser
-        content = chain.invoke({"query": req.query.data})
+        # 4. 创建链应用
+        chain = RunnablePassthrough.assign(
+            history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+        ) | prompt | llm | StrOutputParser()
 
-        # 4. 调用链得到结果
+        # 5. 调用链生成内容
+        chain_input = {"query": req.query.data}
+        content = chain.invoke(chain_input)
+        memory.save_context(chain_input, {"output": content})
+
         return success_json({"content": content})
 
     def ping(self):
