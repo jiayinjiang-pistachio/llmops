adb0020d75d442bef2932e1e018d94b8d432c7f5
root <yin_991@163.com>
2025-12-09 19:05:40 +0800

    feat openai embedding使用示例

--- llmops-api/internal/handler/app_handler.py
+++ llmops-api/internal/handler/app_handler.py
@@ -10,14 +10,17 @@ import os
 import uuid
 from dataclasses import dataclass
 from operator import itemgetter
+from typing import Dict, Any
 from uuid import UUID
 
 from injector import inject
 from langchain.memory import ConversationBufferWindowMemory
 from langchain_community.chat_message_histories import FileChatMessageHistory
+from langchain_core.memory import BaseMemory
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda, RunnableConfig
+from langchain_core.tracers import Run
 from langchain_openai import ChatOpenAI
 
 from internal.exception import FailException
@@ -49,6 +52,24 @@ class AppHandler:
         app = self.appService.delete_app(id)
         return success_message(f"应用已经成功删除，id为{app.id}")
 
+    @classmethod
+    def _load_memory_variables(cls, input: Dict[str, Any], config: RunnableConfig) -> Dict[str, Any]:
+        """加载记忆变量信息"""
+        # 1. 从config中获取configurable
+        configurable = config.get("configurable", {})
+        configurable_memory = configurable.get("memory", None)
+        if configurable_memory is not None and isinstance(configurable_memory, BaseMemory):
+            return configurable_memory.load_memory_variables(input)
+        return {"history": []}
+
+    @classmethod
+    def _save_context(cls, run_obj: Run, config: RunnableConfig) -> None:
+        """存储对应的上下文信息到记忆实体中"""
+        configurable = config.get("configurable", {})
+        configurable_memory = configurable.get("memory", None)
+        if configurable_memory is not None and isinstance(configurable_memory, BaseMemory):
+            configurable_memory.save_context(run_obj.inputs, run_obj.outputs)
+
     def debug(self, app_id: UUID):
         """聊天接口"""
         # 1. 提取从接口中获取的输入
@@ -78,14 +99,15 @@ class AppHandler:
         )
 
         # 4. 创建链应用
-        chain = RunnablePassthrough.assign(
-            history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
-        ) | prompt | llm | StrOutputParser()
+        chain = (RunnablePassthrough.assign(
+            history=RunnableLambda(self._load_memory_variables) | itemgetter("history")
+        ) | prompt | llm | StrOutputParser()).with_listeners(
+            on_end=self._save_context
+        )
 
         # 5. 调用链生成内容
         chain_input = {"query": req.query.data}
-        content = chain.invoke(chain_input)
-        memory.save_context(chain_input, {"output": content})
+        content = chain.invoke(chain_input, config={"configurable": {"memory": memory}})
 
         return success_json({"content": content})
 
---
+++ llmops-api/study/20-OpenAI Embedding使用示例/1-OpenAI嵌入模型示例.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time           : 2025/12/9 18:45
+@Author         : jiayinkong@163.com
+@File           : 1-OpenAI嵌入模型示例.py
+@Description    : 
+"""
+import os
+
+import dotenv
+import numpy as np
+from langchain_openai import OpenAIEmbeddings
+from numpy.linalg import norm
+
+dotenv.load_dotenv()
+
+
+def cosine_similarity(vec1: str, vec2: str) -> float:
+    """计算余弦相似度"""
+    # 1. 计算两个向量的点积
+    dot_product = np.dot(vec1, vec2)
+
+    # 2. 计算两个向量的长度
+    vec1_norm = norm(vec1)
+    vec2_norm = norm(vec2)
+
+    # 3. 计算余弦相似度
+    return dot_product / (vec1_norm * vec2_norm)
+
+
+# 1. 创建文本嵌入模型
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-small",
+    openai_api_key=os.getenv("GPTSAPI_API_KEY"),
+    openai_api_base=os.getenv("OPENAI_API_BASE")
+)
+
+# 2. 嵌入文本
+query_vector = embeddings.embed_query("我是慕小课，我喜欢打篮球")
+# print(query_vector)
+# print(len(query_vector))
+
+# 3. 嵌入文档列表/字符串列表
+document_vector = embeddings.embed_documents([
+    "我叫幕小课，我喜欢打篮球",
+    "这个喜欢打篮球的人叫幕小课",
+    "求知若渴，虚心若愚"
+])
+
+print(len(document_vector))
+
+# 4. 计算余弦相似度
+print("向量1和向量2的相似度：", cosine_similarity(document_vector[0], document_vector[1]))
+print("向量1和向量3的相似度：", cosine_similarity(document_vector[0], document_vector[2]))
