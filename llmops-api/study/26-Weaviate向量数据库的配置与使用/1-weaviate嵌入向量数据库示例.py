06dfb1f6f6de289718cdf57ff16a57e3f8361a5e
root <yin_991@163.com>
2025-12-10 18:42:51 +0800

    feat Weaviate向量数据库配置与使用

---
+++ llmops-api/study/26-Weaviate向量数据库的配置与使用/1-weaviate嵌入向量数据库示例.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time           : 2025/12/10 18:01
+@Author         : jiayinkong@163.com
+@File           : 1-weaviate嵌入向量数据库示例.py
+@Description    : 
+"""
+import os
+
+import dotenv
+import weaviate
+from langchain_openai import OpenAIEmbeddings
+from langchain_weaviate import WeaviateVectorStore
+from weaviate.auth import AuthApiKey
+
+dotenv.load_dotenv()
+
+texts = [
+    "笨笨是一只很喜欢睡觉的猫咪",
+    "我喜欢在夜晚听音乐，这让我感到放松。",
+    "猫咪在窗台上打盹，看起来非常可爱。",
+    "学习新技能是每个人都应该追求的目标。",
+    "我最喜欢的食物是意大利面，尤其是番茄酱的那种。",
+    "昨晚我做了一个梦，梦见自己在太空飞行。",
+    "我的手机突然关机了，让我有些焦虑。",
+    "阅读是我每天都会做的事情，我觉得很充实。",
+    "他们一起计划了一次周末的野餐，希望天气能好。",
+    "我的狗喜欢追逐球，看起来非常开心。",
+]
+
+metadatas = [
+    {"page": 1},
+    {"page": 2},
+    {"page": 3},
+    {"page": 4},
+    {"page": 5},
+    {"page": 6, "account_id": 1},
+    {"page": 7},
+    {"page": 8},
+    {"page": 9},
+    {"page": 10},
+]
+
+# 创建连接客户端
+client = weaviate.connect_to_weaviate_cloud(
+    cluster_url="leskh5srpsgusmg0hyiyg.c0.asia-southeast1.gcp.weaviate.cloud",
+    auth_credentials=AuthApiKey(
+        "TUcxREMvQ2VENG1TSEtUL19JaDh6OXVyVG9uZVRoTzJXUnUwc2l2b0w5bVpzcnRRREF3blZXWEJCRHR3PV92MjAw"),
+    skip_init_checks=True,  # 添加这个，跳过初始化检查
+    additional_config=weaviate.classes.init.AdditionalConfig(
+        timeout=weaviate.classes.init.Timeout(init=30, query=60, insert=120)  # 设置超时
+    )
+)
+
+embedding = OpenAIEmbeddings(
+    model="text-embedding-3-small",
+    openai_api_key=os.getenv("GPTSAPI_API_KEY"),
+    openai_api_base=os.getenv("OPENAI_API_BASE")
+)
+
+# 创建Langchain向量数据库实例
+db = WeaviateVectorStore(
+    client=client,
+    index_name="DatasetDemo",
+    text_key="text",
+    embedding=embedding,
+)
+
+# # 添加数据
+# ids = db.add_texts(texts, metadatas)
+# print("添加文档id列表：", ids)
+
+# # 执行相似性搜索
+# filters = Filter.by_property("page").greater_or_equal(5)
+# print(db.similarity_search_with_score("笨笨", filters=filters))
+
+retriever = db.as_retriever()
+print(retriever.invoke("笨笨"))
