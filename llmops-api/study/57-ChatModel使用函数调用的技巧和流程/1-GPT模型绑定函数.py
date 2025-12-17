44ddd168b3fd1b6cac2ad8406520a04128d22c70
root <yin_991@163.com>
2025-12-17 17:06:31 +0800

    GPT 模型绑定工具函数

---
+++ llmops-api/study/57-ChatModel使用函数调用的技巧和流程/1-GPT模型绑定函数.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time           : 2025/12/17 16:28
+@Author         : jiayinkong@163.com
+@File           : 1-GPT模型绑定函数.py
+@Description    : 
+"""
