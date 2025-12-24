6e7d994a02bf5f8415f8340a19c93034a4b28905
root <yin_991@163.com>
2025-12-24 11:39:29 +0800

    feat llmops的高德工具包插件

---
+++ llmops-api/internal/core/tools/builtin_tools/providers/gaode/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time           : 2025/12/24 11:30
+@Author         : jiayinkong@163.com
+@File           : __init__.py.py
+@Description    : 
+"""
+from .gaode_weather import gaode_weather
+
+__all__ = ["gaode_weather"]
