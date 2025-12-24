6a49dbad1b68e0299411fc36e19ff141dcc26d65
root <yin_991@163.com>
2025-12-24 20:40:14 +0800

    feat 为内置工具接口编写测试用例

--- llmops-api/test/internal/handler/test_app_handler.py
+++ llmops-api/test/internal/handler/test_app_handler.py
@@ -14,9 +14,15 @@ from pkg.response import HttpCode
 class TestAppHandler:
     """APP控制器的测试类"""
 
-    @pytest.mark.parametrize("query", [None, "你好，你是谁?"])
-    def test_completion(self, query, client):
-        resp = client.post("/app/completion", json={"query": query})
+    @pytest.mark.parametrize(
+        "app_id, query",
+        [
+            ("50d5ab41-7319-4c34-8a69-91ff77c3c7dd", None),
+            ("50d5ab41-7319-4c34-8a69-91ff77c3c7dd", "你好，你是？")
+        ]
+    )
+    def test_completion(self, app_id, query, client):
+        resp = client.post(f"/app/{app_id}/debug", json={"query": query})
         assert resp.status_code == 200
         if query is None:
             assert resp.json.get("code") == HttpCode.VALIDATE_ERROR
