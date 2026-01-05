e3963876c2c60eb12454211c2dd224aaae9d80a9
root <yin_991@163.com>
2026-01-05 20:42:53 +0800

    feat 会话记忆简单实现、前端实现SSE流式事件输出

---
+++ llmops-api/internal/service/conversation_service.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time           : 2026/1/5 20:20
+@Author         : jiayinkong@163.com
+@File           : conversation_service.py
+@Description    : 
+"""
