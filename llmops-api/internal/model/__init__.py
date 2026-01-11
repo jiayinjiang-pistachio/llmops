f40c72298ac38bbd5133f55e357d3ac5400ee79c
root <yin_991@163.com>
2026-01-11 11:32:07 +0800

    feat 账号授权相关ORM数据库表

--- llmops-api/internal/model/__init__.py
+++ llmops-api/internal/model/__init__.py
@@ -6,6 +6,7 @@
 @File           : __init__.py.py
 @Description    : 存储数据库相关的类名（数据库中表对应的类信息）
 """
+from .account import Account, AccountOAuth
 from .api_tool import ApiToolProvider, ApiTool
 from .app import App, AppDatasetJoin
 from .conversation import Conversation, Message, MessageAgentThought
@@ -27,4 +28,6 @@ __all__ = [
     "Conversation",
     "Message",
     "MessageAgentThought",
+    "Account",
+    "AccountOAuth",
 ]
