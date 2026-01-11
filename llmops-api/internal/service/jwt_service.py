0d244fd3b4b64c53c53f9a3e7c550a55e320c402
root <yin_991@163.com>
2026-01-11 12:16:02 +0800

    feat jwt服务

---
+++ llmops-api/internal/service/jwt_service.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time           : 2026/1/11 11:57
+@Author         : jiayinkong@163.com
+@File           : jwt_service.py
+@Description    : 
+"""
+import os
+from dataclasses import dataclass
+from typing import Any
+
+import jwt
+from injector import inject
+
+from internal.exception import UnAuthorizationException
+
+
+@inject
+@dataclass
+class JwtService:
+    """jwt服务"""
+
+    @classmethod
+    def generate_token(cls, payload: dict[str, Any]) -> str:
+        """根据传递的载荷信息生成token信息"""
+        secret_key = os.getenv("JWT_SECRET_KEY")
+        return jwt.encode(payload, secret_key, algorithm="HS256")
+
+    @classmethod
+    def parse_token(cls, token: str) -> dict[str, Any]:
+        """解析传入的token信息得到载荷"""
+        secret_key = os.getenv("JWT_SECRET_KEY")
+        try:
+            return jwt.decode(token, secret_key, algorithm=["HS256"])
+        except jwt.ExpiredSignatureError:
+            raise UnAuthorizationException("授权认证凭证已过期，请重新登录")
+        except jwt.InvalidTokenError:
+            raise UnAuthorizationException("解析token出错，请重新登录")
+        except Exception as e:
+            raise e
---
+++ llmops-api/pkg/password/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time           : 2026/1/11 11:57
+@Author         : jiayinkong@163.com
+@File           : __init__.py.py
+@Description    : 
+"""
